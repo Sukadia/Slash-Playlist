6b5f4f84aca80c0e05c2cd0e97bd2aa5f28f22c7
Sukadia <37813632+Sukadia@users.noreply.github.com>
2024-03-31 16:52:15 -0700

    [ext] Automatically load playlists from sidebar
    - Triggers the click event on Youtube's "Show More" so playlists will be loaded and properly fetched without user input

--- src/extension/scripts/find-playlists.ts
+++ src/extension/scripts/find-playlists.ts
@@ -2,6 +2,14 @@ import Browser from "webextension-polyfill"
 
 async function checkPage(){
     console.log("SLASH PLAYLIST")
+
+    // Open playlist dropdown to load playlists
+    const playlistdropdown = document.querySelectorAll("[title=\"Show more\"]")
+    if (playlistdropdown[0]){
+        (playlistdropdown[0].children[0] as HTMLElement).click()
+    }
+
+    // Find playlists starting with "/"
     const playlists = document.querySelectorAll("[href*=\"/playlist\"]")
     let downloadplaylists = []
     for (let playlist of playlists){
