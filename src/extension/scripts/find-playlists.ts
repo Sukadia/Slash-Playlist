fdc19bc6ff7cbb123c797c8811249b54ff3f43ef
Sukadia <37813632+Sukadia@users.noreply.github.com>
2024-04-02 14:35:42 -0700

    [ext] Fix playlist fetching
    - Youtube immediately broke my change, so fetching now must occur on https://www.youtube.com/feed/playlists
    - Currently no UI/automation to indicate this

--- src/extension/scripts/find-playlists.ts
+++ src/extension/scripts/find-playlists.ts
@@ -3,21 +3,18 @@ import Browser from "webextension-polyfill"
 async function checkPage(){
     console.log("SLASH PLAYLIST")
 
-    // Open playlist dropdown to load playlists
-    const playlistdropdown = document.querySelectorAll("[title=\"Show more\"]")
-    if (playlistdropdown[0]){
-        (playlistdropdown[0].children[0] as HTMLElement).click()
-    }
+    // TODO: Automate playlist grabbing via https://www.youtube.com/feed/playlists
+    // Website update requires webpage to be open to see playlists
 
     // Find playlists starting with "/"
-    const playlists = document.querySelectorAll("[href*=\"/playlist\"]")
+    const playlists = document.querySelectorAll("[href*=\"&list\"]")
     let downloadplaylists = []
     for (let playlist of playlists){
-        const playlistname = playlist.getAttribute("title")
+        const playlistname = playlist.children[0].textContent
         if (playlistname?.indexOf("/") == 0){
             console.log(playlistname)
             const playlistlink = playlist.getAttribute("href")!
-            downloadplaylists.push(playlistlink.split("=")[1])
+            downloadplaylists.push(playlistlink.split("&list=")[1].split("&")[0])
         }
     }
     Browser.runtime.sendMessage({
